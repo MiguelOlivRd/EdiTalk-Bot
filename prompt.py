c586c221cfb39c43e8d76ee757113b41d16b60e4
Rodrigues <Miguel.Rodrigues@dellteam.com>
2025-09-20 16:03:04 -0300

    primeira versão funcional

--- prompt.py
+++ prompt.py
@@ -1,8 +1,11 @@
-prompt = """
+def prompt(context, question):
+    return f"""
 Você é um assistente que deve responder perguntas sobre editais da UFCG (Universidade Federal de Campina Grande).
 Um contexto será informado para ajudar a formular a resposta, mas ele nem sempre será adequado.
 O contexto e a pergunta do utilizador são apresentados a seguir.
+O contexto é o edital que mais tem relação com a pergunta.
+Leia o contexto com cuidado. A resposta pode estar nele.
 Contexto = {context}
 Pergunta = {question}
-Se a resposta não estiver no context, responda "Não consigo responder a essa pergunta com minha base de informações".
-""".trim()
+Se a resposta não estiver no contexto, responda "Não consigo responder a essa pergunta com minha base de informações".
+""".strip()
